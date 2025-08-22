a949a37937430faa4e50b303629c7fc277710369
LamTT <lam@anantadigi.com>
2025-08-22 09:29:53 +0700

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
+import fs from "fs";
+import https from "https";
 import app from './app';
 import { connectMongoDB, disconnectMongoDB } from '@/config/mongodb';
 import { closeInfluxDB, checkInfluxDB } from '@/config/influxdb';
@@ -16,6 +18,15 @@ process.on('SIGINT', async () => {
   process.exit(0);
 });
 
-app.listen(PORT, () => {
+const options = {
+  key: fs.readFileSync("./cert/cert-key.pem"),
+  cert: fs.readFileSync("./cert/cert.pem"),
+};
+
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); 
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// }); 
